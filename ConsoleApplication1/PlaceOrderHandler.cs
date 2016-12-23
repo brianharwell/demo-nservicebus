4cfb3f72fabc750ca252580615af802136934c54
Brian Harwell <brianharwell@gmail.com>
2016-12-23 10:35:40 -0500

    Lesson 1 and 2

---
+++ ConsoleApplication1/PlaceOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+
+namespace ConsoleApplication1
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        private static ILog _Logger = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            _Logger.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        private static ILog _Logger = LogManager.GetLogger<Program>();
+
+        public static void Main(string[] args)
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "ClientUI";
+
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            endpointConfiguration.UseTransport<MsmqTransport>();
+            endpointConfiguration.UseSerialization<JsonSerializer>();
+            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            endpointConfiguration.SendFailedMessagesTo("error");
+            endpointConfiguration.EnableInstallers();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+            
+            await RunLoop(endpointInstance);
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                _Logger.Info("Press 'P' to place an order, or 'Q' to quit.");
+
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        _Logger.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+
+                        await endpointInstance.SendLocal(command).ConfigureAwait(false);
+
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        _Logger.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
